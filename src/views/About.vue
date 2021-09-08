1cf6ad8d9da37d064bf3d9291432a364cb864bc6
Yu_Yue <9987jdvrb@gmail.com>
2021-09-08 20:59:19 +0800

    feat: about dialog

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,56 @@
 <template>
   <div class="about">
-    <v-card>
-      <v-card-title>Rules</v-card-title>
-    </v-card>
+    <v-dialog
+      v-model="rule"
+      width="500"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-card 
+          id="card"
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-card-title class="text-center">Rules</v-card-title>
+        </v-card>
+      </template>
+
+      <v-card>
+        <v-card-title class="text-h5 lighten-2">
+          Rule
+        </v-card-title>
+
+        <v-card-text class="text-h6">
+          you are a pig!
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            text
+            @click="rule = false"
+          >
+            OK
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    rule: false,
+  }),
+}
+</script>
+
+<style lang='scss'>
+#card {
+  max-width: 80vw;
+  height: 11vh;
+  margin-left: 10vw;
+  margin-top: 9vh;
+}
+</style>
