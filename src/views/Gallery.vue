079db8bf9e33d13aa74c448e1615bd382b454571
Yu_Yue <9987jdvrb@gmail.com>
2021-09-08 20:47:23 +0800

    feat: gallery

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,14 +1,34 @@
 <template>
   <div>
+    <v-img
+        v-for="(img, id) in imgs"
+        :key="id"
+        id="img"
+        :src="img"
+    ></v-img>
+
+    <v-pagination
+      v-model="page"
+      :length="6"
+    ></v-pagination>
   </div>
 </template>
 
 <script>
 export default {
-
+    data: () => ({
+        page: 1,
+        imgs: [require('@/assets/profile/aurora.jpg'), require('@/assets/profile/cloud-blu.jpg'), 
+                require('@/assets/profile/cloud-org.jpg'), require('@/assets/profile/keyboard.jpg')],
+    }),
 }
 </script>
 
-<style>
-
+<style lang='scss'>
+#img {
+    max-width: 90vw;
+    height: auto;
+    margin-left: 5vw;
+    margin-top: 6vh;
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/pe/' : '/',
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pluginOptions: {
+    dll: {
+      entry: ['vue', 'vuetify', 'vuex', 'vue-axios', 'axios'],
+    },
+  },
 }
